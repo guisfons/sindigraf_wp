a64518349e64dc34f4de3041e4446b91602f94ae
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-11 23:38:33 -0300

    ajuste aside

--- www/wp-content/themes/sindigraf/template-parts/sections/content-aside.php
+++ www/wp-content/themes/sindigraf/template-parts/sections/content-aside.php
@@ -1,3 +1,7 @@
-<aside class="aside <?php if(get_field( 'tamanho_aside' ) == 'grande') {echo 'aside-large';} ?>" style="background-image: url(<?php echo get_field('imagem_de_fundo'); ?>)">
-    <h2><?php echo get_field('titulo_aside'); ?></h2>
+<?php
+$aside = get_field('aside');
+if( $aside ): ?>
+<aside class="aside <?php if(get_field( 'aside_tamanho_aside' ) == 'grande') {echo 'aside-large';} ?>" style="background-image: url(<?php echo get_field('aside_imagem_de_fundo'); ?>)">
+    <h2><?php echo get_field('aside_titulo_aside'); ?></h2>
 </aside>
+<?php endif; ?>

90d59606844b6b1f64585fc840c9121454a36605
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-13 11:08:30 -0300

    testye

--- www/wp-content/themes/sindigraf/page-templates/juridico.php
+++ www/wp-content/themes/sindigraf/page-templates/juridico.php
@@ -19,32 +19,7 @@ get_header();
     </article>
   </section>
   <section class="wrapper juridico-arquivos">
-    <?php
-    $args = array(
-      'post_parent'    => $post->ID,
-      'order'          => 'ASC',
-      'orderby'        => 'menu_order'
-    );
-
-
-    $parent = new WP_Query($args);
-
-    if ($parent->have_posts()) : ?>
-
-      <?php while ($parent->have_posts()) : $parent->the_post(); ?>
-
-        <div id="parent-<?php the_ID(); ?>" class="parent-page">
-
-          <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-
-          <p><?php the_advanced_excerpt(); ?></p>
-
-        </div>
-
-      <?php endwhile; ?>
-
-    <?php endif;
-    wp_reset_postdata(); ?>
+    
   </section>
 </main>
 <?php get_footer(); ?>

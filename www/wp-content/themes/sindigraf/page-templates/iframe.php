684d07c74613acc8ee28e0f052e52a7ed50b1e44
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-14 01:56:52 -0300

    tela iframe

---
+++ www/wp-content/themes/sindigraf/page-templates/iframe.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Iframe
+ * Template Post Type: page
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+<style>
+    body {
+        overflow-y: hidden;
+    }
+</style>
+<main>
+    <iframe width="100%" height="100%" src="https://spmidiahost.com.br/jobs-abrigaf" frameborder="0"></iframe>
+</main>
+<?php get_footer(); ?>
